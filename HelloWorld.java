773debef2854e89a1cf30c181a6d273ce7804e2a
swatisk1990@yahoo.com <swatisk1990@yahoo.com>
2022-08-09 12:20:10 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+package com.learn.java.intro; 
+class HelloWorld{
+	public static void main(String args[]){
+		 System.out.println("Hello world");
+	}
+}
---
+++ Summation.java
@@ -0,0 +1,13 @@
+package com.learn.java.intro;
+import java.util.*;
+class Summation{
+	public static void main(String args[]){
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter 2 integers");
+		int a,b;
+		a=s.nextInt();
+		b=s.nextInt();
+		System.out.println("SUM\t: "+(a+b));
+	}
+}
+ 
